a8666251c50f8d820f79db4dd3a883e91d0e4f86
DINESH MANNAM <dineshmannam@dineshs-mbp.myfiosgateway.com>
2021-02-19 13:00:51 -0500

    created subnets for all az's in that region

--- subnets.tf
+++ subnets.tf
@@ -1,8 +1,9 @@
-resource "aws_subnet" "coreinfra_tf_subnet1" {
+resource "aws_subnet" "coreinfra_tf_subnets" {
+  count      = length(var.azs)
   vpc_id     = aws_vpc.coreinfra_tf_vpc.id
   cidr_block = var.subnet_cidr
 
   tags = {
-    Name = "tf_subnet1"
+    Name = "tf_subnet"
   }
 }
--- vars.tf
+++ vars.tf
@@ -9,3 +9,8 @@ variable "vpc_cidr" {
 variable "subnet_cidr" {
   default = "190.160.1.0/24"
 }
+
+variable "azs" {
+  type    = list(string)
+  default = ["us-west-2a", "us-west-2b", "us-west-2c"]
+}
